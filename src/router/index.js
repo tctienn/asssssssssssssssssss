53fefc78d47aaa3f5f0ba17a092a65c011a0adf6
tctienn <mainichichimvm@gmail.com>
2024-05-01 00:05:08 +0700

    s

--- src/router/index.js
+++ src/router/index.js
@@ -1,330 +1,148 @@
-import { h, resolveComponent } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-
-import DefaultLayout from '@/layouts/DefaultLayout'
-
-const routes = [
+export default [
   {
-    path: '/',
-    name: 'Home',
-    component: DefaultLayout,
-    redirect: '/dashboard',
-    children: [
-      {
-        path: '/dashboard',
-        name: 'Dashboard',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-          import(
-            /* webpackChunkName: "dashboard" */ '@/views/dashboard/Dashboard.vue'
-          ),
-      },
-      {
-        path: '/product',
-        name: 'product',
-        component: {
-          render() {
-            return h(resolveComponent('router-view'))
-          },
-        },
-        redirect: '/product/list',
-        children: [
-          {
-            path: '/product/list',
-            name: 'list',
-            component: () => import('@/views/product/Products.vue'),
-          },
-          {
-            path: '/product/add',
-            name: 'addProduct',
-            component: () => import('@/views/product/addProduct.vue'),
-          },
-          {
-            path: '/product/catergory',
-            name: 'catergory',
-            component: () => import('@/views/product/Catergory.vue'),
-          },
-          {
-            path: '/product/tag',
-            name: 'tag',
-            component: () => import('@/views/product/Tag.vue'),
-          },
-          {
-            path: '/product/brand',
-            name: 'brand',
-            component: () => import('@/views/product/Brand.vue'),
-          },
-          {
-            path: '/product/size',
-            name: 'size',
-            component: () => import('@/views/product/Size.vue'),
-          },
-        ]
-      },
-      {
-        path: '/theme',
-        name: 'Theme',
-        redirect: '/theme/typography',
-      },
-      {
-        path: '/theme/colors',
-        name: 'Colors',
-        component: () => import('@/views/theme/Colors.vue'),
-      },
-      {
-        path: '/theme/typography',
-        name: 'Typography',
-        component: () => import('@/views/theme/Typography.vue'),
-      },
-      {
-        path: '/base',
-        name: 'Base',
-        component: {
-          render() {
-            return h(resolveComponent('router-view'))
-          },
-        },
-        redirect: '/base/breadcrumbs',
-        children: [
-          {
-            path: '/base/accordion',
-            name: 'Accordion',
-            component: () => import('@/views/base/Accordion.vue'),
-          },
-          {
-            path: '/base/breadcrumbs',
-            name: 'Breadcrumbs',
-            component: () => import('@/views/base/Breadcrumbs.vue'),
-          },
-          {
-            path: '/base/cards',
-            name: 'Cards',
-            component: () => import('@/views/base/Cards.vue'),
-          },
-          {
-            path: '/base/carousels',
-            name: 'Carousels',
-            component: () => import('@/views/base/Carousels.vue'),
-          },
-          {
-            path: '/base/collapses',
-            name: 'Collapses',
-            component: () => import('@/views/base/Collapses.vue'),
-          },
-          {
-            path: '/base/list-groups',
-            name: 'List Groups',
-            component: () => import('@/views/base/ListGroups.vue'),
-          },
-          {
-            path: '/base/navs',
-            name: 'Navs',
-            component: () => import('@/views/base/Navs.vue'),
-          },
-          {
-            path: '/base/paginations',
-            name: 'Paginations',
-            component: () => import('@/views/base/Paginations.vue'),
-          },
-          {
-            path: '/base/placeholders',
-            name: 'Placeholders',
-            component: () => import('@/views/base/Placeholders.vue'),
-          },
-          {
-            path: '/base/popovers',
-            name: 'Popovers',
-            component: () => import('@/views/base/Popovers.vue'),
-          },
-          {
-            path: '/base/progress',
-            name: 'Progress',
-            component: () => import('@/views/base/Progress.vue'),
-          },
-          {
-            path: '/base/spinners',
-            name: 'Spinners',
-            component: () => import('@/views/base/Spinners.vue'),
-          },
-          {
-            path: '/base/tables',
-            name: 'Tables',
-            component: () => import('@/views/base/Tables.vue'),
-          },
-          {
-            path: '/base/tooltips',
-            name: 'Tooltips',
-            component: () => import('@/views/base/Tooltips.vue'),
-          },
-        ],
-      },
-      {
-        path: '/buttons',
-        name: 'Buttons',
-        component: {
-          render() {
-            return h(resolveComponent('router-view'))
-          },
-        },
-        redirect: '/buttons/standard-buttons',
-        children: [
-          {
-            path: '/buttons/standard-buttons',
-            name: 'Buttons',
-            component: () => import('@/views/buttons/Buttons.vue'),
-          },
-          {
-            path: '/buttons/dropdowns',
-            name: 'Dropdowns',
-            component: () => import('@/views/buttons/Dropdowns.vue'),
-          },
-          {
-            path: '/buttons/button-groups',
-            name: 'Button Groups',
-            component: () => import('@/views/buttons/ButtonGroups.vue'),
-          },
-        ],
-      },
-      {
-        path: '/forms',
-        name: 'Forms',
-        component: {
-          render() {
-            return h(resolveComponent('router-view'))
-          },
-        },
-        redirect: '/forms/form-control',
-        children: [
-          {
-            path: '/forms/form-control',
-            name: 'Form Control',
-            component: () => import('@/views/forms/FormControl.vue'),
-          },
-          {
-            path: '/forms/select',
-            name: 'Select',
-            component: () => import('@/views/forms/Select.vue'),
-          },
-          {
-            path: '/forms/checks-radios',
-            name: 'Checks & Radios',
-            component: () => import('@/views/forms/ChecksRadios.vue'),
-          },
-          {
-            path: '/forms/range',
-            name: 'Range',
-            component: () => import('@/views/forms/Range.vue'),
-          },
-          {
-            path: '/forms/input-group',
-            name: 'Input Group',
-            component: () => import('@/views/forms/InputGroup.vue'),
-          },
-          {
-            path: '/forms/floating-labels',
-            name: 'Floating Labels',
-            component: () => import('@/views/forms/FloatingLabels.vue'),
-          },
-          {
-            path: '/forms/layout',
-            name: 'Layout',
-            component: () => import('@/views/forms/Layout.vue'),
-          },
-          {
-            path: '/forms/validation',
-            name: 'Validation',
-            component: () => import('@/views/forms/Validation.vue'),
-          },
-        ],
-      },
-      {
-        path: '/charts',
-        name: 'Charts',
-        component: () => import('@/views/charts/Charts.vue'),
-      },
-
-      {
-        path: '/notifications',
-        name: 'Notifications',
-        component: {
-          render() {
-            return h(resolveComponent('router-view'))
-          },
-        },
-        redirect: '/notifications/alerts',
-        children: [
-          {
-            path: '/notifications/alerts',
-            name: 'Alerts',
-            component: () => import('@/views/notifications/Alerts.vue'),
-          },
-          {
-            path: '/notifications/badges',
-            name: 'Badges',
-            component: () => import('@/views/notifications/Badges.vue'),
-          },
-          {
-            path: '/notifications/modals',
-            name: 'Modals',
-            component: () => import('@/views/notifications/Modals.vue'),
-          },
-          {
-            path: '/notifications/toasts',
-            name: 'Toasts',
-            component: () => import('@/views/notifications/Toasts.vue'),
-          },
-        ],
-      },
-      {
-        path: '/widgets',
-        name: 'Widgets',
-        component: () => import('@/views/widgets/Widgets.vue'),
-      },
-    ],
-  },
-  {
-    path: '/pages',
-    redirect: '/pages/404',
-    name: 'Pages',
-    component: {
-      render() {
-        return h(resolveComponent('router-view'))
-      },
+    component: 'CNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+    badge: {
+      color: 'primary',
+      text: 'admin',
     },
-    children: [
-      {
-        path: '404',
-        name: 'Page404',
-        component: () => import('@/views/pages/Page404'),
-      },
-      {
-        path: '500',
-        name: 'Page500',
-        component: () => import('@/views/pages/Page500'),
-      },
-      {
-        path: 'login',
-        name: 'Login',
-        component: () => import('@/views/pages/Login'),
-      },
-      {
-        path: 'register',
-        name: 'Register',
-        component: () => import('@/views/pages/Register'),
-      },
-    ],
   },
+  {
+    component: 'CNavTitle',
+    name: 'DỮ LIỆU BÁN HÀNG',
+  },
+  {
+    component: 'CNavGroup',
+    name: 'Product',
+    to: '/product',
+    icon: 'cil-puzzle',
+    items: [
+      {
+        component: 'CNavItem',
+        name: 'Danh sách sản phẩm',
+        to: '/product/list',
+      }, {
+        component: 'CNavItem',
+        name: 'Tạo sản phẩm',
+        to: '/product/add',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Danh mục (catergory)',
+        to: '/product/catergory',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Nhãn (tag)',
+        to: '/product/tag',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Thương hiệu (brand)',
+        to: '/product/brand',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Kích cỡ (size)',
+        to: '/product/size',
+      },
+    ]
+  },
+  ////blog
+  {
+    component: 'CNavGroup',
+    name: 'blog',
+    to: '/blog',
+    icon: 'cil-calculator',
+    items: [
+
+      {
+        component: 'CNavItem',
+        name: 'danh sách blog',
+        to: '/blog/list',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Tạo blog mới',
+        to: '/blog/add',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Quản lý blog hiển thị',
+        to: '/blog/blog-control',
+      },
+
+
+    ]
+  },
+  //
+  //// hóa đơn
+  {
+    component: 'CNavTitle',
+    name: 'Thanh toán',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Invloices',
+    to: '/invoice/list',
+    icon: 'cil-notes',
+    items: [
+      {
+        component: 'CNavItem',
+        name: 'InvoiceWait',
+        to: '/invoice/wait',
+        icon: 'cil-note-add',
+      },
+      {
+        component: 'CNavItem',
+        name: 'list',
+        to: '/invoice/list',
+        icon: 'cil-note-add',
+      }
+    ]
+  },
+
+  //////// 
+  {
+    component: 'CNavTitle',
+    name: 'Tài khoản',
+  },
+  {
+    component: 'CNavItem',
+    name: 'accout',
+    to: '/user/list',
+    icon: 'cil-notes',
+    items: [
+      {
+        component: 'CNavItem',
+        name: 'accout',
+        to: '/user/list',
+        icon: 'cil-note-add',
+      }
+
+    ]
+  },
+
+
+
+
+
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Download CoreUI',
+  //   href: 'http://coreui.io/vue/',
+  //   icon: { name: 'cil-cloud-download', class: 'text-white' },
+  //   _class: 'bg-success text-white',
+  //   target: '_blank'
+  // },
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Try CoreUI PRO',
+  //   href: 'http://coreui.io/pro/vue/',
+  //   icon: { name: 'cil-layers', class: 'text-white' },
+  //   _class: 'bg-danger text-white',
+  //   target: '_blank'
+  // }
 ]
-
-const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
-  routes,
-  scrollBehavior() {
-    // always scroll to top
-    return { top: 0 }
-  },
-})
-
-export default router
